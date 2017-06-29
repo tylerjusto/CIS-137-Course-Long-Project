4432da92a8b2d0c440e805487c5f215ce2d4aac8
tylerjusto <tbjusto2014@gmail.com>
2017-06-28 22:11:54 -0400

    src

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Tanning Oasis</h2>
+        </div>
+        <p className="App-intro">
+          Hello & Welcome
+	<div class="w3-bar w3-black">
+  		<li><a href="#" class="w3-bar-item w3-button">Home</a></li>
+		<li><a href="#" class="w3-bar-item w3-button">Membership</a></li>
+		<li><a href="#" class="w3-bar-item w3-button">About Us</a></li>
+	</div>        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
